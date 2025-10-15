db2fc26a3cfc77097b5a4b801e33e7dd368fc13e
Roel Simons <roel.simons@simonix.nl>
2025-10-15 14:25:41 +0200

    BME280 added

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,16 @@
 #include <SoftwareSerial.h>
 #include <LoRa.h>
 #include <WiFi.h>
+#include <Adafruit_BME280.h>
 
 #define LORA_FREQ 433E6
+#define SEALEVELPRESSURE_HPA (1013.25)
+
 
 HardwareSerial hs(2); // UART2
 SdsDustSensor sds(hs); //  additional parameters: retryDelayMs and maxRetriesNotAvailable
+SPIClass spi(VSPI);
+Adafruit_BME280 bme;
 
 const int MINUTE = 60000;
 const int WAKEUP_WORKING_TIME = MINUTE; // 30 seconds.
@@ -16,6 +21,7 @@ const int MEASUREMENT_INTERVAL = 2 * MINUTE;
 void publish_alive();
 void publish_pm_data(float pm25, float pm10);
 void sendMessage(String outgoing);
+void displayAndSendBmeValues();
 
 unsigned long lastRun = 0;
 
@@ -25,6 +31,8 @@ char sMacAddr[18];
 void setup() {
   delay(500);
   Serial.begin(115200);
+
+  Wire.begin(SDA, SCL);
 
     // Get deviceId
   uint8_t macAddr[6];
@@ -42,6 +50,14 @@ void setup() {
     ESP.restart();
   }
 
+  Serial.println("Looking for sensor");
+  if (! bme.begin(BME280_ADDRESS_ALTERNATE)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+  Serial.println("BME280 sensor found");
+
   LoRa.setPreambleLength(8);
   LoRa.setSpreadingFactor(7);
   LoRa.setSignalBandwidth(125E3);
@@ -57,6 +73,13 @@ void setup() {
   Serial.print("SDS011 ");
   Serial.println(sds.queryFirmwareVersion().toString()); // prints firmware version
   Serial.println(sds.setQueryReportingMode().toString()); // ensures sensor is in 'query' reporting mode
+
+  // suggested rate is 1/60Hz (1m)
+  bme.setSampling(Adafruit_BME280::MODE_FORCED,
+                  Adafruit_BME280::SAMPLING_X1, // temperature
+                  Adafruit_BME280::SAMPLING_X1, // pressure
+                  Adafruit_BME280::SAMPLING_X1, // humidity
+                  Adafruit_BME280::FILTER_OFF   );
 }
 
 void loop() {
@@ -84,6 +107,9 @@ void loop() {
     } else {
       Serial.println("SDS011 sensor is sleeping");
     }
+
+    bme.takeForcedMeasurement();
+    displayAndSendBmeValues();
 
     lastRun = millis();
   }
@@ -118,6 +144,37 @@ void publish_pm_data(float pm25, float pm10) {
   sendMessage(payload);
 }
 
+void displayAndSendBmeValues() {
+
+  String temp = String(bme.readTemperature());
+  String pressure = String(bme.readPressure() / 100);
+  String humidity = String(bme.readHumidity());
+  String altitude = String(bme.readAltitude(SEALEVELPRESSURE_HPA));
+
+  String payload = "";
+  payload += "{\"temperature\":";
+  payload += temp;
+  payload += ",\"pressure\":";
+  payload += pressure;
+  payload += ",\"humidity\":";
+  payload += humidity;
+  payload += ",\"altitude\":";
+  payload += altitude;
+  payload += ",\"sensor\":";
+  payload += "\"bme280\"";
+  payload += ",\"device\":";
+  payload += "\"";
+  payload += ssid;
+  payload += "\"";
+  payload += ",\"mac\":";
+  payload += "\"";
+  payload += sMacAddr;
+  payload += "\"";
+  payload += "}";
+
+  sendMessage(payload);
+}
+
 
 void publish_alive() {
 
@@ -141,4 +198,6 @@ void sendMessage(String outgoing) {
   LoRa.beginPacket();                   // start packet
   LoRa.print(outgoing);                 // add payload
   LoRa.endPacket();                     // finish packet and send it
+  Serial.println(outgoing);
+
 }
